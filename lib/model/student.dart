af6487c2c40cedea03c771da1a1eff388d4b6c65
ChewanRai1 <220154@softwarica.edu.np>
2024-05-03 11:24:21 +0545

    student add feature and the list and delete

---
+++ lib/model/student.dart
@@ -0,0 +1,7 @@
+class StudentModel{
+  String? firstName;
+  String? lastName;
+  String? city;
+
+  StudentModel({this.firstName, this.lastName, this.city});
+}
--- lib/screen/student_details_screen.dart
+++ lib/screen/student_details_screen.dart
@@ -1,4 +1,7 @@
+// ignore: depend_on_referenced_packages
+// import 'package:batch_32b/model/student.dart';
 import 'package:flutter/material.dart';
+import 'package:studentmanagement/model/student.dart';
 
 class StudentDetailsScreen extends StatefulWidget {
   const StudentDetailsScreen({super.key});
@@ -8,99 +11,129 @@ class StudentDetailsScreen extends StatefulWidget {
 }
 
 class _StudentDetailsScreenState extends State<StudentDetailsScreen> {
-  // final _gap =
-  String? city;
-  final items = [
+  final _gap = const SizedBox(height: 10);
+
+  final _fnameController = TextEditingController(text: 'Chewan');
+  final _lnameController = TextEditingController(text: 'Rai');
+  final TextEditingController _cityController = TextEditingController();
+
+  List<DropdownMenuItem> cities = [
     const DropdownMenuItem(
-      value: 'Kathmandu',
-      child: Text('Kathmandu'),
+      value: 'Kathmandu', // timile select garda
+      child: Text('Kathmandu'), // user lai dekhaune
     ),
-    const DropdownMenuItem(
-      value: 'Pokhara',
-      child: Text('Pokhara'),
-    ),
-    const DropdownMenuItem(
-      value: 'Chitwan',
-      child: Text('Chitwan'),
-    ),
+    const DropdownMenuItem(value: 'Pokhara', child: Text('Pokhara')),
+    const DropdownMenuItem(value: 'Dharan', child: Text('Dharan')),
   ];
-  // List <Student> student = [];
+
+  final _key = GlobalKey<FormState>();
+
+  List<StudentModel> students = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Student Management'),
-        backgroundColor: Colors.blue,
+        title: const Text('Student Details'),
       ),
-      // body: DropdownButtonFormField(
-      //   icon: const Icon(Icons.arrow_downward),
-      //   items: items,
-      //   onChanged: (value) {
-      //     setState(() {
-      //       city = value;
-      //     });
-      //   },
-      //   hint: const Text('Select City'),
-      //   decoration: const InputDecoration(
-      //     labelText: 'Select City',
-      //     border: OutlineInputBorder(),
-      //   ),
-      //   validator: (value) {
-      //     if (value == null) {
-      //       return 'Please select city';
-      //     }
-      //     return null;
-      //   },
-      // ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 10,
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+          key: _key,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _fnameController,
+                decoration: const InputDecoration(
+                  hintText: 'First Name',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter fname';
+                  }
+                  return null;
+                },
+              ),
+              _gap,
+              TextFormField(
+                controller: _lnameController,
+                decoration: const InputDecoration(
+                  hintText: 'Last Name',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter lname';
+                  }
+                  return null;
+                },
+              ),
+              _gap,
+              DropdownButtonFormField(
+                items: cities,
+                onChanged: (value) {
+                  _cityController.text = value.toString();
+                },
+                decoration: const InputDecoration(
+                  hintText: 'Select city',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please select city';
+                  }
+                  return null;
+                },
+              ),
+              _gap,
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_key.currentState!.validate()) {
+                      final student = StudentModel(
+                        firstName: _fnameController.text,
+                        lastName: _lnameController.text,
+                        city: _cityController.text,
+                      );
+                      setState(() {
+                        students.add(student);
+                      });
+                    }
+                  },
+                  child: const Text('Add Student'),
+                ),
+              ),
+              _gap,
+              const Text(
+                'Student List',
+                style: TextStyle(fontSize: 20),
+              ),
+              students.isEmpty
+                  ? const Text('No data')
+                  : Expanded(
+                      child: ListView.builder(
+                        itemCount: students.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: const Icon(Icons.person),
+                            title: Text(students[index].firstName!),
+                            subtitle: Text(students[index].city!),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {
+                                setState(() {
+                                  students.removeAt(index);
+                                });
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+            ],
           ),
-          TextFormField(
-            decoration: const InputDecoration(
-              label: Text('Enter a first name'),
-              border: OutlineInputBorder(),
-            ),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-                label: Text('Enter a last name'), border: OutlineInputBorder()),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          DropdownButtonFormField(
-            icon: const Icon(Icons.arrow_downward),
-            items: items,
-            onChanged: (value) {
-              setState(() {
-                city = value;
-              });
-            },
-            hint: const Text('Select City'),
-            decoration: const InputDecoration(
-              labelText: 'Select City',
-              border: OutlineInputBorder(),
-            ),
-            validator: (value) {
-              if (value == null) {
-                return 'Please select city';
-              }
-              return null;
-            },
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text('Add Student'),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
